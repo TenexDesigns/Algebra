7dda8eebdc3895dcb3848439387ef25038cb6484
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-01 22:23:08 +0300

    Create 9_Domain and Range.java

---
+++ 9_Domain and Range.java
@@ -0,0 +1,72 @@
+The text you provided accurately describes the concepts of domain and range in algebra. Let's break down the key points:
+
+### Domain and Range:
+
+1. **Definition:**
+   - **Domain:** The set of all possible input values (x-values) for which the function is defined.
+   - **Range:** The set of all possible output values (y-values) corresponding to the input values in the domain.
+
+2. **Application:**
+   - Domain and range are fundamental concepts in algebra and are used to describe the input and output behavior of functions. They are crucial for understanding the scope and limitations of a function.
+
+### Explanation of the Text:
+
+1. **"When you look at a function, you see x and you see y!":** This emphasizes the two variables, x and y, which are typically associated with a function.
+
+2. **"Domain is all the x values, and range is all the ys":** Correct. The domain consists of all possible x-values, and the range consists of all possible y-values.
+
+3. **"Only values that exist should be in its sight!":** This highlights that the domain and range should only include values that make the function well-defined.
+
+4. **"Functions like x squared and cubed have infinite domain and range":** True. Quadratic and cubic functions often have domains and ranges that span all real numbers.
+
+5. **"Exclude undefined x or y values from the domain or range":** This is an important rule. If there are values of x or y for which the function is undefined, they should be excluded from the domain or range, respectively.
+
+6. **"Functions like the square root of x can't have negative x":** Correct. The square root of a negative number is undefined in the real number system.
+
+7. **"Some complex functions never reach a certain y":** This refers to situations where certain y-values may not be attainable due to the nature of the function.
+
+### Conclusion:
+
+The text provides a concise and accurate overview of domain and range in the context of algebra. Understanding these concepts is crucial for analyzing and interpreting the behavior of functions in various mathematical applications.
+
+
+
+
+--------------------------------------------------------------------------------------------
+
+
+  Certainly! Let's go through a couple of examples to illustrate the concepts of domain and range.
+
+### Example 1:
+
+Consider the function \( f(x) = x^2 \).
+
+**Domain:** All real numbers are allowed as inputs, so the domain is \( \mathbb{R} \) (all real numbers).
+
+**Range:** The function \( f(x) = x^2 \) can produce all non-negative values. The range is the set of all non-negative real numbers, denoted as \( [0, \infty) \).
+
+### Example 2:
+
+Let \( g(x) = \sqrt{x} \).
+
+**Domain:** The square root of a negative number is undefined in the real number system. Therefore, the domain of \( g(x) \) is all non-negative real numbers, denoted as \( [0, \infty) \).
+
+**Range:** The output of \( g(x) \) is always non-negative, so the range is also \( [0, \infty) \).
+
+### Example 3:
+
+Consider the function \( h(x) = \frac{1}{x} \).
+
+**Domain:** The denominator cannot be zero, so the domain excludes \( x = 0 \). The domain is \( \mathbb{R} \setminus \{0\} \) (all real numbers except zero).
+
+**Range:** As \( x \) approaches zero from the positive side, \( h(x) \) approaches positive infinity, and as \( x \) approaches zero from the negative side, \( h(x) \) approaches negative infinity. Therefore, the range is \( (-\infty, 0) \cup (0, \infty) \).
+
+These examples illustrate how to determine the domain and range of functions and emphasize the importance of considering restrictions based on the nature of the functions.
+
+
+  
+
+
+
+
+  
