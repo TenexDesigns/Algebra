702b47278a186a6f6effd841c88947842ce67695
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-01 22:00:04 +0300

    Create 6_Point_and_Slopr_Problem.java

---
+++ 6_Point_and_Slopr_Problem.java
@@ -0,0 +1,36 @@
+To sketch a line given a point and a slope, you can use the point-slope form of the equation of a line:
+
+\[ y - y_1 = m(x - x_1) \]
+
+where \((x_1, y_1)\) is the given point, and \(m\) is the slope.
+
+In this case, the given point is \((4, 1)\), and the slope is \(2/1\).
+
+So, substituting these values into the point-slope form:
+
+\[ y - 1 = \frac{2}{1}(x - 4) \]
+
+Now, simplify the equation:
+
+\[ y - 1 = 2(x - 4) \]
+
+Distribute the 2 on the right side:
+
+\[ y - 1 = 2x - 8 \]
+
+Add 1 to both sides:
+
+\[ y = 2x - 7 \]
+
+Now you have the equation of the line. You can use this equation to sketch the line on a coordinate plane. Alternatively, you can use the slope and the given point to plot additional points and connect them to create the line.
+
+Here's how you can plot a few points and sketch the line:
+
+1. Use the given point \((4, 1)\).
+2. Use the slope \(2/1\) to find another point. For example, move 1 unit to the right and 2 units up from the given point to get another point \((5, 3)\).
+3. Connect the two points to sketch the line.
+
+Your line should pass through both points \((4, 1)\) and \((5, 3)\), representing the equation \(y = 2x - 7\).
+
+
+  
