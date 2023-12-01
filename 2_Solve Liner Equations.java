6fe4410728f6040a3df7829e8ae35ae80291cec3
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-01 21:21:28 +0300

    Create 2_Solve Liner Equations.java

---
+++ 2_Solve Liner Equations.java
@@ -0,0 +1,72 @@
+The statement you provided offers a poetic and mnemonic way of expressing the process of solving linear equations in algebra. Let me break down the key steps mentioned in the statement:
+
+1. **Equations as a balanced scale:** This analogy is commonly used to help students understand that both sides of an equation must remain equal. Whatever operation is performed on one side must also be performed on the other to maintain balance.
+
+2. **Variables as placeholders:** Variables (like 'x' in typical algebraic equations) represent unknown values. The goal is to find the value of the variable that satisfies the equation.
+
+3. **Isolation of the variable:** To solve for the variable, the goal is often to isolate it on one side of the equation. This is achieved by performing operations in reverse.
+
+4. **Order of operations in reverse:** When manipulating the equation, operations are performed in reverse order. For example, if there's addition, you perform subtraction; if there's multiplication, you perform division. exponents, Parents, for pairs like (+- and * /), the one closer to  the left is first
+
+Here's a step-by-step guide on solving linear equations:
+
+**Example: Solve for x in the equation 2x + 5 = 11**
+
+1. **Isolate the variable:** Subtract 5 from both sides to isolate the term with the variable.
+   \[ 2x + 5 - 5 = 11 - 5 \]
+   \[ 2x = 6 \]
+
+2. **Solve for the variable:** Divide both sides by 2 to solve for x.
+   \[ \frac{2x}{2} = \frac{6}{2} \]
+   \[ x = 3 \]
+
+The poem you provided captures the essence of this process in a creative way, reinforcing the importance of maintaining balance on both sides of the equation while isolating and solving for the variable.
+
+
+
+  REVERS OPERATIONS
+  -------------------------------------------------------
+
+The "reverse order of operations" refers to the process of undoing the operations in an equation in the reverse order of the typical order of operations. The standard order of operations is often remembered using the acronym PEMDAS:
+
+1. **P - Parentheses**
+2. **E - Exponents**
+3. **M/D - Multiplication and Division (from left to right)**
+4. **A/S - Addition and Subtraction (from left to right)**
+
+When solving an equation, you work backward through these steps to isolate the variable. Here's what the statement means in the context of reversing the order of operations:
+
+1. **If there's addition in the original equation, you perform subtraction in reverse:** If you added a constant to one side of the equation, you subtract that constant from both sides to isolate the variable.
+
+2. **If there's multiplication in the original equation, you perform division in reverse:** If you multiplied the variable by a constant, you divide both sides by that constant to isolate the variable.
+
+3. **Exponents:** If there are exponents in the equation, you take the reverse operation, which is finding the root. For example, if you squared a variable, you would take the square root in reverse.
+
+4. **Parentheses:** If there are parentheses in the equation, you undo them. For example, if you added or subtracted within parentheses, you perform the opposite operation to eliminate the parentheses.
+
+The idea is to work backward through the operations that were performed on the variable, essentially undoing each step to isolate the variable on one side of the equation.
+
+Let's look at an example to illustrate:
+
+**Example: Solve for x in the equation 3x + 7 = 16**
+
+1. **Isolate the variable:** Subtract 7 (reverse of addition) from both sides.
+   \[ 3x + 7 - 7 = 16 - 7 \]
+   \[ 3x = 9 \]
+
+2. **Solve for the variable:** Divide by 3 (reverse of multiplication) on both sides.
+   \[ \frac{3x}{3} = \frac{9}{3} \]
+   \[ x = 3 \]
+
+In this example, we worked backward through the operations: subtraction (reverse of addition) and division (reverse of multiplication).
+
+
+
+
+
+
+
+
+
+
+  
