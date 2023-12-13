aed9a559e735b2724fc7d171e6c68f34b389e9ca
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-13 20:29:48 +0300

    Create Rational and Irrational numbers.java

---
+++ Rational and Irrational numbers/Rational and Irrational numbers.java
@@ -0,0 +1,25 @@
+Let's address the nature of \( \frac{22}{7} \) and the mathematical constant \( \pi \) in terms of rationality or irrationality.
+
+### \( \frac{22}{7} \):
+\( \frac{22}{7} \) is a rational number. It is a fraction where both the numerator (22) and the denominator (7) are integers. Rational numbers can be expressed as the ratio of two integers, and they can either terminate or repeat in decimal form. In this case, \( \frac{22}{7} \) is a rational number that is commonly used as an approximation for the mathematical constant \( \pi \).
+
+### \( \pi \):
+The mathematical constant \( \pi \) is an irrational number. It cannot be expressed as the ratio of two integers, and its decimal representation is non-terminating and non-repeating. The value of \( \pi \) is approximately 3.14159 and continues indefinitely without a repeating pattern.
+
+### Distinction between Rational and Irrational Numbers:
+
+1. **Rational Numbers:**
+   - Can be expressed as the ratio of two integers (\( \frac{a}{b} \) where \( a \) and \( b \) are integers and \( b \neq 0 \)).
+   - Their decimal representations either terminate (e.g., \( \frac{3}{4} = 0.75 \)) or repeat (e.g., \( \frac{1}{3} = 0.333... \)).
+
+2. **Irrational Numbers:**
+   - Cannot be expressed as the ratio of two integers.
+   - Their decimal representations are non-terminating and non-repeating.
+
+### Rational and Irrational Numbers Beyond Square Roots:
+While square roots are a common source of irrational numbers (e.g., \( \sqrt{2} \)), there are many other examples of irrational numbers that do not involve square roots. For instance, \( \pi \) and the mathematical constant \( e \) (the base of natural logarithms) are famous examples of irrational numbers that are not related to square roots.
+
+In summary, the distinction between rational and irrational numbers is not limited to square roots. Any number that can be expressed as the ratio of two integers is rational, while those that cannot are considered irrational. Square roots are just one category of irrational numbers.
+
+
+  .....
