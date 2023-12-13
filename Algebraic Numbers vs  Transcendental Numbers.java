fdb9e3646ecb72705ab6ce2302c87c72e35c6480
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-13 20:28:42 +0300

    Create Algebraic Numbers vs  Transcendental Numbers.java

---
+++ Algebraic Numbers vs  Transcendental Numbers.java
@@ -0,0 +1,15 @@
+**Algebraic Numbers:**
+Algebraic numbers are numbers that are solutions to polynomial equations with integer coefficients. In other words, an algebraic number is a root of a polynomial with coefficients that are integers. Algebraic numbers include rational numbers, integers, square roots of non-negative integers, cube roots, and so on. For example, the square root of 2 is algebraic because it is the solution to the polynomial equation \(x^2 - 2 = 0\).
+
+**Transcendental Numbers:**
+Transcendental numbers are numbers that are not algebraic. In other words, they are not solutions to any polynomial equation with integer coefficients. Transcendental numbers "transcend" algebraic relationships. Famous examples of transcendental numbers include the mathematical constant \(e\) (the base of natural logarithms) and \(\pi\) (the ratio of the circumference of a circle to its diameter).
+
+**Distinction:**
+The key distinction between algebraic and transcendental numbers lies in their relationship to polynomial equations. Algebraic numbers can be expressed as roots of such equations, while transcendental numbers cannot. Transcendental numbers are not confined to the solutions of any polynomial equation and have more profound mathematical properties.
+
+**Significance:**
+Transcendental numbers are not necessarily related to transcending mathematics; rather, the term "transcendental" refers to their transcendence over algebraic relationships. These numbers often arise in more advanced mathematical contexts, such as analysis and the study of functions, and they have intriguing properties. The discovery of the transcendental nature of certain numbers was a significant development in the history of mathematics.
+
+
+
+  ...
