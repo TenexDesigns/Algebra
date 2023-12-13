208cfda5dfec9a83e836a3017170623b619915f4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-13 21:09:34 +0300

    Create 1_Perfect Squares.java

---
+++ Perfect squares/1_Perfect Squares.java
@@ -0,0 +1,65 @@
+Perfect Square
+A perfect square is a number that can be expressed as the product of an integer by itself or as the second exponent of an integer. For example, 25 is a perfect square because it is the product of integer 5 by itself, 5 × 5 = 25. However, 21 is not a perfect square number because it cannot be expressed as the product of two same integers.
+
+
+
+How to Identify Perfect Squares?
+  
+Observe the last digit of the perfect square numbers 1 to 20 as given in the table above. 
+  You will notice that they end with any one of these digits 0, 1, 4, 5, 6, or 9.
+  
+  After trying various perfect square numbers you would have observed an important property of perfect squares.
+  Numbers that have any of the digits 2, 3, 7, or 8 in their units place are non-perfect square numbers, whereas, 
+  numbers that have any of the digits 0, 1, 4, 5, 6, or 9 in their units place might be perfect squares.
+
+  
+  The following observations can be made to identify a perfect square.
+
+The numbers ending with 3 and 7 will have 9 as the units place digit in its square number.
+The number ending with 5 will have 5 as its units place digit in its square number.
+The number ending with 4 and 6 will have 6 as the units place digit in its square number.
+The number ending with 2 and 8 will have 4 as the units place digit in its square number.
+The numbers ending with 1 and 9 will have 1 as the units place digit in its square number.
+  
+Let us look at a few deviations from the above-defined rules of a perfect square number. The numbers 159 and 169 both end with the digit 9 but 169 is a perfect square, whereas 159 is not. If the number ends with the digit 0, then you may look for the following: How many zeros are there at the end of the number? Let's say we have a number 1000. If there is an odd number of zeros, then it's definitely not a perfect square. 1000 has 3 zeros at the end. Thus, it's not a perfect square. If there are an even number of zeros, then it might be a perfect square. 400 and 300 both have an even number of zeros at the end, but 400 = 202, which is a perfect square, but 300 is not a square of any whole number.
+
+
+
+  Perfect Squares List
+The table given below shows the perfect squares of the first 20 natural numbers. The first column shows the natural number and the second column shows the square of the natural number. You can easily find the square of a natural number by multiplying it by itself. For example, 2 × 2 = 4, 3 × 3 = 9, 4 × 4 = 16, and so on.
+
+Natural Number	Perfect Square
+
+Number                 Perfect Square       
+1                        	1
+2                        	4
+3                        	9
+4                        	16
+5                        	25
+6                        	36
+7                        	49
+8                        	64
+9                        	81
+10                        100
+11	                      121
+12	                      144
+13	                      169
+14	                      196
+15	                      225
+16	                      256
+17	                      289
+18	                      324
+19	                      361
+20	                      400
+
+
+  
+Look at these lists of perfect squares 1-100 to have a better understanding of perfect square numbers 1 to 100.
+
+
+
+
+  
+
+
+  
