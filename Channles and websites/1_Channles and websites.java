83cb16fcdb9d9a7a3cdafdc00278fa9a23719d3b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-14 01:28:11 +0300

    Create 1_Channles and websites.java

---
+++ Channles and websites/1_Channles and websites.java
@@ -0,0 +1,5 @@
+https://testbook.com/maths/algebraic-function
+https://www.mathsisfun.com/algebra/rational-expression.html
+https://www.mathsisfun.com/algebra/polynomials-solving.html
+https://www.mathsisfun.com/algebra/polynomials-division-long.html
+
