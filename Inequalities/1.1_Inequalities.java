c6bfea979d67b0120b79a0922ffece2075d47730
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-14 17:39:05 +0300

    Create 1.1_Inequalities.java

---
+++ Inequalities/1.1_Inequalities.java
@@ -0,0 +1,79 @@
+Inequalities are statements that compare two mathematical expressions using symbols like < (less than), > (greater than), ≤ (less than or equal to), and ≥ (greater than or equal to). Unlike equations that seek a specific value for the unknown variable, inequalities focus on a range of values that satisfy the comparison.
+
+Here's a breakdown of inequalities in algebra:
+
+**1. Understanding the symbols:**
+
+* **< (less than):** The first expression is smaller than the second. Example: 3 < 5
+* **> (greater than):** The first expression is larger than the second. Example: 7 > 2
+* **≤ (less than or equal to):** The first expression is either smaller than or equal to the second. Example: 4 ≤ 4
+* **≥ (greater than or equal to):** The first expression is either larger than or equal to the second. Example: 9 ≥ 6
+
+**2. Solving inequalities:**
+
+Solving inequalities involves isolating the variable and finding the range of values that make the comparison true. Here are some key steps:
+
+* **Isolate the variable:** Use basic algebraic operations like addition, subtraction, multiplication, and division to manipulate the inequality and get the variable by itself. Remember, performing the same operation on both sides of the inequality symbol maintains its direction.
+* **Consider the inequality symbol:** Depending on the symbol, the direction of the inequality might flip when performing certain operations. For example, multiplying/dividing both sides by a negative number reverses the direction (< becomes >, and vice versa).
+* **Graph the solution:** Plotting the inequality on a number line can visually represent the range of solutions. Mark the excluded regions based on the inequality symbol and identify the range of values on the allowed side.
+
+**3. Rules for solving inequalities:**
+
+* **Adding/subtracting the same number:** Add or subtract the same number to both sides without changing the direction of the inequality.
+* **Multiplying/dividing by a positive number:** Multiply or divide both sides by a positive number without changing the direction.
+* **Multiplying/dividing by a negative number:** Multiplying/dividing both sides by a negative number flips the direction of the inequality.
+
+**4. Types of inequalities:**
+
+* **Linear inequalities:** These involve expressions with variables raised to the power of 1. Example: 2x + 3 > 5
+* **Quadratic inequalities:** These involve expressions with variables raised to the power of 2. Example: x^2 - 5x + 6 ≥ 0
+* **Absolute value inequalities:** These involve expressions with absolute values. Example: |x - 3| < 2
+
+**5. Applications of inequalities:**
+
+Inequalities have diverse applications in various fields, including:
+
+* **Economics:** Modeling resource allocation, market trends, and investment decisions.
+* **Physics:** Describing motion, forces, and energy relationships.
+* **Chemistry:** Analyzing chemical reactions, concentrations, and equilibrium constants.
+* **Computer science:** Optimizing algorithms and resource management in computing systems.
+
+Remember, practice is key to mastering inequalities! By understanding the concepts, applying the rules, and tackling various practice problems, you'll gain confidence in solving these essential mathematical expressions.
+
+I hope this explanation provides a comprehensive overview of inequalities in algebra. Feel free to ask if you have any further questions or need specific examples!
+
+
+
+
+  
+----------------------------------------------------------------------------------------------------------
+
+
+  Inequalities in algebra are a comparison between two numbers, values, or expressions. They can be less than, greater than, less than or equal to, or greater than or equal to each other. The symbols used to make these comparisons are:
+
+- `<` for less than
+- `>` for greater than
+- `≤` for less than or equal to
+- `≥` for greater than or equal to
+- `=` for equal
+- `≠` for not equal
+
+For example, `x < 5` is read as 'x is less than 5'. The solution set is the numbers less than 5. Similarly, `x ≤ 5` is read as 'x is less than or equal to 5'. The solution set is the numbers less than or equal to 5. `x > 5` is read as 'x is greater than 5'. The solution set is all the numbers greater than 5. `x ≥ 5` is read as 'x is greater than or equal to 5'. The solution set is all the numbers greater than or equal to 5 [Source 3](https://thirdspacelearning.com/us/math-resources/topic-guides/algebra/inequalities/).
+
+To solve inequalities, you can often add (or subtract) a number from both sides. For example, if you have the inequality `x - 3 < -5`, you can clear out the "/2" by multiplying both sides by 2. Because you are multiplying by a positive number, the inequalities will not change [Source 0](https://www.mathsisfun.com/algebra/inequality-solving.html).
+
+When you multiply or divide both sides of an inequality by a negative number, the inequality symbol flips. For example, if you have `3 < 5` and you multiply both sides by `-1`, you will get `-3 > -5`. This is no longer a true inequality comparison. In order to make it true, you will have to flip the inequality symbol, so `<` becomes `>` [Source 3](https://thirdspacelearning.com/us/math-resources/topic-guides/algebra/inequalities/).
+
+For more complex inequalities, you can use the following process:
+
+1. Write the inequality as an equation.
+2. Solve the equation. You can use any process of solving quadratic equations.
+3. Represent the solutions from Step 2 on the number line and identify the intervals. Take care of open circles and closed circles.
+4. Take a random number from each interval and check whether the inequality is true for that number.
+5. The inequalities with "true" from the above table are solutions [Source 4](https://www.cuemath.com/algebra/inequalities/).
+
+Remember, the way you would solve a linear inequality follows the same process as you would do to solve a linear equation. With equations, you choose a side of the equation to bring the variable to, and you would do the same for an inequality. It’s most common to have the variable on the left-hand side of an equality and the integers or real numbers on the right side [Source 3](https://thirdspacelearning.com/us/math-resources/topic-guides/algebra/inequalities/).
+                                                                                                                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                                                                             
+  ....
