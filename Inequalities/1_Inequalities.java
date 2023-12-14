c31b327fa5d1d38eaf94a0aa44338822de2a9dfc
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-14 17:19:48 +0300

    Create 1_Inequalities.java

---
+++ Inequalities/1_Inequalities.java
@@ -0,0 +1,29 @@
+Inequalities are mathematical expressions that describe the relationship between two expressions, indicating whether one is greater than, less than, greater than or equal to, less than or equal to, or not equal to the other. The symbols used in inequalities include:
+
+- \(>\) : greater than
+- \(<\) : less than
+- \(\geq\) : greater than or equal to
+- \(\leq\) : less than or equal to
+- \(\neq\) : not equal to
+
+Here are some common rules and methods for solving inequalities:
+
+1. **Addition and Subtraction**: You can add or subtract the same quantity from both sides of an inequality without changing its direction. For example, if \(a < b\), then \(a + c < b + c\) and \(a - c < b - c\).
+
+2. **Multiplication and Division**: When you multiply or divide both sides of an inequality by a positive number, the direction of the inequality remains the same. However, if you multiply or divide by a negative number, the direction of the inequality is reversed. For example, if \(a < b\) and \(c > 0\), then \(ac < bc\). But if \(c < 0\), then \(ac > bc\).
+
+3. **Combining Inequalities**: You can combine two or more inequalities using the logical operators "and" (\(\cap\)) and "or" (\(\cup\)). For example, if \(a < b\) and \(b < c\), then \(a < b < c\).
+
+4. **Solving Compound Inequalities**: Compound inequalities involve more than one inequality. For example, \(a < x < b\) is a compound inequality. To solve such inequalities, you often break them down into simpler inequalities and solve each part separately.
+
+5. **Absolute Value Inequalities**: For absolute value inequalities, you often consider cases based on whether the expression inside the absolute value is positive or negative.
+
+6. **Graphical Representation**: Inequalities can be graphically represented on a number line. Solutions to inequalities are often intervals on the number line.
+
+Remember to be careful with the direction of the inequality when you multiply or divide by a negative number, as it will reverse the inequality. Also, keep in mind that the solution to an inequality may be a range of values rather than a single value.
+
+Solving inequalities is an essential skill in algebra and is often used in various mathematical and real-world applications.
+
+
+
+  .....
